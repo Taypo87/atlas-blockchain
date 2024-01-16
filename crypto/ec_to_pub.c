9ed7fd269d2a02011e774ad312cd9db225c94551
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-16 17:13:36 -0600

    task 2

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -2,15 +2,10 @@
 
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
-    //EC_POINT const *point;
-    //const EC_GROUP *group;
-    //point_conversion_form_t form;
     BN_CTX *ctx;
 
     ctx = BN_CTX_new();
-    //point = EC_KEY_get0_public_key(key);
-   // group = EC_KEY_get0_group(key);
-    //form = EC_KEY_get_conv_form(key);
+ 
     EC_KEY_oct2key((EC_KEY *)key, pub, EC_PUB_LEN, ctx);
     return(pub);
     

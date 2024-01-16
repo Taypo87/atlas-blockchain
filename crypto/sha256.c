802191291f3680ddee7d111f9ffb5d7191cef90c
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-16 12:38:18 -0600

    task 0

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -2,14 +2,18 @@
 
 /**
  * sha256 - creates a sha hash from given input
+ * @s:	the string to be hashed
+ * @len: digest length
+ * @digest: contains the hash
+ * Return: the digest
 */
 uint8_t *sha256(int8_t const *s, size_t len,
-            uint8_t digest[SHA256_DIGEST_LENGTH])
+			uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-    SHA256_CTX ctx;
+	SHA256_CTX ctx;
 
-    SHA256_Init(&ctx);
-    SHA256_Update(&ctx, (void *)s, len);
-    SHA256_Final(digest, &ctx);
-    return (digest);
+	SHA256_Init(&ctx);
+	SHA256_Update(&ctx, (void *)s, len);
+	SHA256_Final(digest, &ctx);
+	return (digest);
 }

3a825d9841a52b374872ae66cae9476b8c9ba5c9
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-17 03:34:53 -0600

    task 2

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -1,12 +1,19 @@
 #include "hblk_crypto.h"
-
+/**
+ * ec_to_pub - converts and reutrns a public key
+ * @key: they private/public key struct
+ * pub: buffer to store the public key
+ * Return: pub
+*/
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
-    BN_CTX *ctx;
+	EC_POINT const *public_point;
+	EC_GROUP const *group;
+	BN_CTX *ctx;
 
-    ctx = BN_CTX_new();
- 
-    EC_KEY_oct2key((EC_KEY *)key, pub, EC_PUB_LEN, ctx);
-    return(pub);
-    
+	ctx = BN_CTX_new();
+	public_point = EC_KEY_get0_public_key(key);
+	group = EC_KEY_get0_group(key);
+	EC_POINT_point2oct(group, public_point, POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, ctx);
+	return(pub);
 }

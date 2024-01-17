312cc50068831e71c60ee72370b70891e52a9fd2
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-17 04:36:57 -0600

    task 3

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -7,8 +7,17 @@
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
 	EC_KEY *key;
+	EC_POINT *p;
+	EC_GROUP const *group;
+	BN_CTX *ctx;
 
 	key = EC_KEY_new();
-	EC_KEY_set_public_key(key, pub);
+	ctx = BN_CTX_new();
+	group = EC_GROUP_new_by_curve_name(EC_CURVE);
+	EC_KEY_set_group(key, group);
+	p = EC_POINT_new(group);
+	EC_POINT_oct2point(group, p, pub, (EC_PUB_LEN), ctx);
+	EC_KEY_set_public_key(key, p);
+
 	return (key);
 }

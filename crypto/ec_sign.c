0adc2937847a7481bce96d7d038b8b732e64f72f
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-19 05:51:47 -0600

    betty and NULL checks

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -5,16 +5,19 @@
  * @msg: thebytes to sign
  * @msglen: length of the byte array
  * @sig: sig struct that contains the signature and the sig length
+ * Return: the signature
 */
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 								 size_t msglen, sig_t *sig)
 {
 	u_int32_t sig_len = 0;
 
+	if (!key || !msg)
+		return (NULL);
 	if (ECDSA_sign(0, msg, msglen, sig->sig, &sig_len, (EC_KEY *)key))
 	{
 		sig->len = sig_len;
-		return(sig->sig);
+		return (sig->sig);
 	}
 	return (NULL);
 }

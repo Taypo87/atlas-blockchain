3de1006267b7479983d1e671046f61e2eab2cfab
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-19 05:49:18 -0600

    task 6

---
+++ crypto/ec_sign.c
@@ -0,0 +1,20 @@
+#include "hblk_crypto.h"
+/**
+ * ec_sign - creates a signature from a given set of bytes
+ * @key: the EC_KEY pair
+ * @msg: thebytes to sign
+ * @msglen: length of the byte array
+ * @sig: sig struct that contains the signature and the sig length
+*/
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
+								 size_t msglen, sig_t *sig)
+{
+	u_int32_t sig_len = 0;
+
+	if (ECDSA_sign(0, msg, msglen, sig->sig, &sig_len, (EC_KEY *)key))
+	{
+		sig->len = sig_len;
+		return(sig->sig);
+	}
+	return (NULL);
+}

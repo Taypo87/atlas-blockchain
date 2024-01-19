d24fba8d42b04ea68cd4b03c90168168c083132b
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-19 06:20:25 -0600

    added check for msglen and betty fixes

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -10,7 +10,7 @@
 int ec_verify(EC_KEY const *key, uint8_t const *msg,
 						size_t msglen, sig_t const *sig)
 {
-	if (!key || !msg || !sig || msglen = 0)
+	if (!key || !msg || !sig || msglen == 0)
 		return (0);
 	if (ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *)key))
 		return (1);

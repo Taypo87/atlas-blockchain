b72c32e619c056c0fe809f2c1693182d02507581
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-19 06:19:51 -0600

    added check for msglen and betty fixes

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -2,14 +2,15 @@
 /**
  * ec_verify - verifies an ec signature
  * @key: the EC_KEY struct holding the key pair
- * @msg: the message 
+ * @msg: the message
  * @msglen: message length
  * @sig: struct containing the signature and sig length
  * Return: 1 on success, else 0
 */
-int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t const *sig)
+int ec_verify(EC_KEY const *key, uint8_t const *msg,
+						size_t msglen, sig_t const *sig)
 {
-	if (!key || !msg || !sig)
+	if (!key || !msg || !sig || msglen = 0)
 		return (0);
 	if (ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *)key))
 		return (1);

60292aacb8d4430e17a2bd940b5d00c4eff4e1a0
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-31 10:12:19 -0600

    task 0

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -2,8 +2,8 @@
 
 blockchain_t *blockchain_create(void)
 {
-    blockchain_t *blockchain = calloc(sizeof(blockchain_t), 1);
-    block_t *genesis_block = calloc(sizeof(block_t), 1);
+    blockchain_t *blockchain = calloc(1, sizeof(blockchain_t));
+    block_t *genesis_block = calloc(1, sizeof(block_t));
     block_info_t gen_block_info;
     block_data_t gen_block_data;
     uint8_t genesis_hash[BLOCKCHAIN_DATA_MAX] = "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03";
@@ -22,7 +22,7 @@ blockchain_t *blockchain_create(void)
     genesis_block->info = gen_block_info;
     memcpy(genesis_block->hash, genesis_hash, BLOCKCHAIN_DATA_MAX);
 
-    blockchain->chain = llist_create(MT_SUPPORT_FALSE);
+    blockchain->chain = llist_create(MT_SUPPORT_TRUE);
     llist_add_node(blockchain->chain, genesis_block, ADD_NODE_FRONT);
     return (blockchain);
 

7c041d0c65a63eca5aa83c17daea95f79add473a
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-31 11:16:42 -0600

    reworked the code

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -7,7 +7,7 @@ blockchain_t *blockchain_create(void)
     blockchain_t *blockchain = calloc(1, sizeof(blockchain_t));
     block_t *genesis_block = calloc(1, sizeof(block_t));
     uint8_t data[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', ' ', 'S', 'c', 'h', 'o', 'o', 'l', '\0'};
-    uint8_t gensis_hash = "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03";
+    int8_t gensis_hash = "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03";
 
     genesis_block->info.timestamp = 1537578000;
 

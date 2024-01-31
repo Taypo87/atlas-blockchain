baed32debcaf79272aeaef582d994a32fd791bd4
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-31 10:34:06 -0600

    reworked the code

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -4,22 +4,16 @@ blockchain_t *blockchain_create(void)
 {
     blockchain_t *blockchain = calloc(1, sizeof(blockchain_t));
     block_t *genesis_block = calloc(1, sizeof(block_t));
-    block_info_t gen_block_info;
-    block_data_t gen_block_data;
     uint8_t genesis_hash[BLOCKCHAIN_DATA_MAX] = "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03";
     uint8_t data[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', ' ', 'S', 'c', 'h', 'o', 'o', 'l', '\0'};
 
-    gen_block_info.difficulty = 0;
-    gen_block_info.index = 0;
-    gen_block_info.nonce = 0;
-    gen_block_info.timestamp = 1537578000;
-    memset(gen_block_info.prev_hash, 0, BLOCKCHAIN_DATA_MAX); 
-    
-    gen_block_data.len = (uint32_t)(sizeof(data) - 1);
-    memcpy(gen_block_data.buffer, data, gen_block_data.len);
+    genesis_block->info.timestamp = 1537578000;
 
-    genesis_block->data = gen_block_data;
-    genesis_block->info = gen_block_info;
+    memset(genesis_block->info.prev_hash, 0, BLOCKCHAIN_DATA_MAX); 
+    
+    genesis_block->data.len = (uint32_t)(sizeof(data) - 1);
+    memcpy(genesis_block->data.buffer, data, genesis_block->data.len);
+
     memcpy(genesis_block->hash, genesis_hash, BLOCKCHAIN_DATA_MAX);
 
     blockchain->chain = llist_create(MT_SUPPORT_TRUE);

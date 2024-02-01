513ac8898f8d751e76a794c57580607fd31e35b9
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-01 11:13:22 -0600

    changing stuff around

--- blockchain/v0.1/blockchain_destroy.c
+++ blockchain/v0.1/blockchain_destroy.c
@@ -2,8 +2,8 @@
 
 void blockchain_destroy(blockchain_t *blockchain)
 {
-	void (*blk_destroy_ptr)(block_t *);
+	void (*blk_destroy_ptr)(block_t *); 
 
 	blk_destroy_ptr = block_destroy;
-	llist_destroy((llist_t *)blockchain, llist_size((llist_t *)blockchain), (node_dtor_t)blk_destroy_ptr);
+	llist_destroy((llist_t *)blockchain, llist_size((llist_t *)blockchain), NULL);
 }

587222f72ad6ac56edfa1e5d5208f692fe907935
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-01 04:52:39 -0600

    task 3

--- blockchain/v0.1/blockchain_destroy.c
+++ blockchain/v0.1/blockchain_destroy.c
@@ -5,6 +5,6 @@
 void blockchain_destroy(blockchain_t *blockchain)
 {
 	void (*blk_dstry_ptr)(block_t *);
-	blk_dstry_ptr = block_destroy();
+	blk_dstry_ptr = block_destroy;
 	llist_destroy(blockchain, llist_size(blockchain), blk_dstry_ptr);
 }

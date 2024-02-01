acb364479768b0cf6908f3d328cfed1147624c78
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-01 04:08:52 -0600

    betty

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -1,20 +1,20 @@
 #include "blockchain.h"
 #include <time.h>
-
+/**
+ * block_create - creates a new block in the blockchain
+ * @prev: pointer to the previous block
+ * @data: data to be stored in the new block
+ * @data_len: length of data
+*/
 block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len)
 {
-    block_t *new_block = calloc(1, sizeof(block_t));
+	block_t *new_block = calloc(1, sizeof(block_t));
 
-    memcpy(new_block->data.buffer, data, BLOCKCHAIN_DATA_MAX);
-    new_block->data.len = data_len;
+	memcpy(new_block->data.buffer, data, BLOCKCHAIN_DATA_MAX);
+	new_block->data.len = data_len;
+	new_block->info.index = prev->info.index + 1;
+	new_block->info.timestamp = time(NULL);
+	memcpy(new_block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
 
-/*
-    might not need this line since we used calloc
-    memset(new_block->hash, 0, SHA256_DIGEST_LENGTH);
-*/
-    new_block->info.index = prev->info.index + 1;
-    new_block->info.timestamp = time(NULL);
-    memcpy(new_block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
-
-    return (new_block);
+	return (new_block);
 }

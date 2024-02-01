20739e024311f319637c9c5d0a4cec649653b7f9
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-01 05:06:43 -0600

    typecasting

--- blockchain/v0.1/blockchain_destroy.c
+++ blockchain/v0.1/blockchain_destroy.c
@@ -2,7 +2,7 @@
 
 void blockchain_destroy(blockchain_t *blockchain)
 {
-	void (*blk_destroy_ptr)(block_t);
+	void (*blk_destroy_ptr)(block_t *);
 
 	blk_destroy_ptr = block_destroy;
 	llist_destroy((llist_t *)blockchain, llist_size((llist_t *)blockchain), (node_dtor_t)blk_destroy_ptr);

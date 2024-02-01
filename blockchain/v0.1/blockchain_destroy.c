3317bb0c08fae96c4926ebaf100eca09742f90b9
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-01 11:28:59 -0600

    changing stuff around

--- blockchain/v0.1/blockchain_destroy.c
+++ blockchain/v0.1/blockchain_destroy.c
@@ -1,5 +1,9 @@
 #include "blockchain.h"
 
+/**
+ * blockchain_destroy - frees a blockchain
+ * @blockchain: pointer to the blockchain to free
+*/
 void blockchain_destroy(blockchain_t *blockchain)
 {
 	llist_destroy((llist_t *)blockchain->chain, 

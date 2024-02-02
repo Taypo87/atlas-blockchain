7700b9cc235ded1ab8ea992e2dbf305d0f3c18b2
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-02 04:09:08 -0600

    task 4

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -7,7 +7,7 @@
 */
 uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-    SHA256_CTX *c;
+    SHA256_CTX *c = NULL;
 
     SHA256_Init(c);
     SHA256_Update(c, (void *)block, SHA256_DIGEST_LENGTH);

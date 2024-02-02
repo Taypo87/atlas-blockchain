1b3b5149a3d3b94dbd90141ab1ad5b335eceff92
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-02 04:30:13 -0600

    converted to hex string

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -9,12 +9,13 @@ uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]
 {
     SHA256_CTX c;
     char hex_str[SHA256_DIGEST_LENGTH * 2 + 1];
+    int i;
 
     SHA256_Init(&c);
     SHA256_Update(&c, (void *)block, SHA256_DIGEST_LENGTH);
     SHA256_Final(hash_buf, &c);
     
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
     {
         sprintf(&hex_str[i * 2], "%02x", hash_buf[i]);
     }

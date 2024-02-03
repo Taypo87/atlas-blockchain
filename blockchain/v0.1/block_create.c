6fc062851b818c43f5ee1331d1a8eeaf849aedb6
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-03 03:22:48 -0600

    changed timestamp

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -10,12 +10,14 @@
 block_t *block_create(block_t const *prev, int8_t const *data,
 												uint32_t data_len)
 {
+	time_t current;
 	block_t *new_block = calloc(1, sizeof(block_t));
 
+	current = time(NULL);
 	memcpy(new_block->data.buffer, data, data_len);
 	new_block->data.len = data_len;
 	new_block->info.index = prev->info.index + 1;
-	new_block->info.timestamp = 1537578000;
+	new_block->info.timestamp = current;
 	memcpy(new_block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
 
 	return (new_block);

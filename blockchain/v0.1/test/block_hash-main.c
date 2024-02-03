6ea07a97340278e51b9b844598ed06f495952ce0
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-03 03:09:59 -0600

    updated main file to checker's main

--- blockchain/v0.1/test/block_hash-main.c
+++ blockchain/v0.1/test/block_hash-main.c
@@ -4,8 +4,21 @@
 
 #include "blockchain.h"
 
-void _blockchain_print(blockchain_t const *blockchain);
-void _blockchain_destroy(blockchain_t *blockchain);
+/**
+ * _print_hex_buffer - Prints a buffer in its hexadecimal form
+ *
+ * @buf: Pointer to the buffer to be printed
+ * @len: Number of bytes from @buf to be printed
+ */
+static void _print_hex_buffer(uint8_t const *buf, size_t len)
+{
+	size_t i;
+
+	for (i = 0; buf && i < len; i++)
+		printf("%02x", buf[i]);
+
+	fflush(NULL);
+}
 
 /**
  * main - Entry point
@@ -14,21 +27,22 @@ void _blockchain_destroy(blockchain_t *blockchain);
  */
 int main(void)
 {
-    blockchain_t *blockchain;
-    block_t *block;
+	blockchain_t *blockchain;
+	block_t *block;
+	uint8_t hash[SHA256_DIGEST_LENGTH];
 
-    blockchain = blockchain_create();
-    block = llist_get_head(blockchain->chain);
+	blockchain = blockchain_create();
+	block = llist_get_head(blockchain->chain);
 
-    block = block_create(block, (int8_t *)"Holberton", 9);
-    llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
-    _blockchain_print(blockchain);
+	block = block_create(block, (int8_t *)"Holberton", 9);
+	block->info.timestamp = 972;
+	llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
 
-    block_hash(block, block->hash);
-    block = block_create(block, (int8_t *)"School", 6);
-    llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
-    _blockchain_print(blockchain);
+	block_hash(block, hash);
+	printf("Hash: ");
+	_print_hex_buffer(hash, SHA256_DIGEST_LENGTH);
+	printf("\n");
 
-    blockchain_destroy(blockchain);
-    return (EXIT_SUCCESS);
+	blockchain_destroy(blockchain);
+	return (EXIT_SUCCESS);
 }

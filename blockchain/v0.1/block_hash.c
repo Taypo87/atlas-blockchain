b257c057298264fae3fc28603c0596d3f18a04df
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-07 02:49:33 -0600

    passed info and data seperatly to update()

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -10,7 +10,8 @@ uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]
     SHA256_CTX c;
 
     SHA256_Init(&c);
-    SHA256_Update(&c, (void *)block, SHA256_DIGEST_LENGTH);
+    SHA256_Update(&c,  (uint8_t const *)&(block->info), sizeof(block->info));
+    SHA256_Update(&c,  (uint8_t const *)&(block->info), block->data.len);
     SHA256_Final(hash_buf, &c);
     return (hash_buf);
     

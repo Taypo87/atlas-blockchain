ddfbb4702e639d2e46aeb543314a6f8eec87cc11
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-07 02:52:52 -0600

    passed info and data seperatly to update()

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -5,14 +5,25 @@
  * @hash_buf; the buffer to store the hash
  * Return: pointer to the hash_buf
 */
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
-{
-    SHA256_CTX c;
+#include "blockchain.h"
 
-    SHA256_Init(&c);
-    SHA256_Update(&c,  (uint8_t const *)&(block->info), sizeof(block->info));
-    SHA256_Update(&c,  (uint8_t const *)&(block->info), block->data.len);
-    SHA256_Final(hash_buf, &c);
-    return (hash_buf);
-    
+/**
+ * block_hash - Computes the hash of a block using SHA-256 algorithm
+ * @block: Pointer to the block to be hashed
+ * @hash_buf: Buffer to store the resulting hash
+ *
+ * Return: Pointer to the computed hash buffer
+ */
+
+uint8_t *block_hash(block_t const *block,
+					uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+	SHA256_CTX c;
+
+	SHA256_Init(&c);
+	SHA256_Update(&c, (uint8_t const *)&(block->info), sizeof(block->info));
+	SHA256_Update(&c, (uint8_t const *)&(block->data), (block->data.len));
+
+	SHA256_Final(hash_buf, &c);
+	return (hash_buf);
 }

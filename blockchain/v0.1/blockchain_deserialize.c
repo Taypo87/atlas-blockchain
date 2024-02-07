dedfd133866916de082cff8bb01e3d83c224d255
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-07 11:49:22 -0600

    task 6

---
+++ blockchain/v0.1/blockchain_deserialize.c
@@ -0,0 +1,56 @@
+#include "blockchain.h"
+#include "provided/endianness.h"
+
+static int check_num(unsigned char *magic_num)
+{
+    unsigned char static_num[4] = {'H', 'B', 'L', 'K'};
+    int i = 0;
+    while (i < 4)
+    {
+        if (magic_num[i] != static_num[i])
+            return (0);
+        i++;
+    }
+    return (1);
+
+}
+
+static int check_version(unsigned char *version)
+{
+    unsigned char static_num[3] = {'0', '.', '1'};
+    int i = 0;
+    while (i < 3)
+    {
+        if (version[i] != static_num[i])
+            return (0);
+        i++;
+    }
+    return (1);
+
+}
+
+blockchain_t *blockchain_deserialize(char const *path)
+{
+    FILE *fp;
+    blockchain_t *blockchain = calloc(1, sizeof(blockchain_t));
+    block_t block;
+    unsigned char four_[4], eight_[8], thirtytwo_[32];
+
+    fp = fopen(path, "rb");
+    if (!fp)
+        return (NULL);
+    fread(four_, 1, 4, fp);
+    if (!check_num(four_))
+        return (NULL);
+    fread(four_, 1, 3, fp);
+    if (!check_version(four_))
+        return (NULL);
+    fread(four_, 1, 1, fp);
+    if (_get_endianness() != four_[0])
+        puts("endianness does not match\n");
+    fread(four_, 1, 4, fp);
+    printf("%d\n", four_);
+    return (blockchain);
+
+    
+}

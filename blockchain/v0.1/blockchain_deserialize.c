bcc424a5260219772b1253e42f23ab1c0fc08d6e
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-16 15:35:07 -0600

    fixed up the segfault

--- blockchain/v0.1/blockchain_deserialize.c
+++ blockchain/v0.1/blockchain_deserialize.c
@@ -1,75 +1,58 @@
 #include "blockchain.h"
-#include "provided/endianness.h"
 
-static int check_num(unsigned char *magic_num)
+/**
+ * write_blocks - Deserialize blocks from a file and add them to a blockchain
+ * @size: Number of blocks to deserialize
+ * @fd: File descriptor of the file containing serialized blocks
+ * @chain: Pointer to the blockchain where blocks will be added
+ *
+ * Return: Pointer to the updated blockchain
+ */
+
+static blockchain_t *write_blocks(uint32_t size, FILE *fd, blockchain_t *chain)
 {
-    unsigned char static_num[4] = {'H', 'B', 'L', 'K'};
-    int i = 0;
-    while (i < 4)
-    {
-        if (magic_num[i] != static_num[i])
-            return (0);
-        i++;
-    }
-    return (1);
+	block_t *block;
+	uint32_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		block = calloc(1, sizeof(block_t));
+		fread(&block->info, sizeof(block_info_t), 1, fd);
+		fread(&(block->data.len), sizeof(uint8_t), 4, fd);
+		fread(block->data.buffer, sizeof(uint8_t), block->data.len, fd);
+		fread(block->hash, sizeof(uint8_t), SHA256_DIGEST_LENGTH, fd);
+		llist_add_node(chain->chain, block, ADD_NODE_REAR);
+	}
+
+	return (chain);
 
 }
 
-static int check_version(unsigned char *version)
-{
-    unsigned char static_num[3] = {'0', '.', '1'};
-    int i = 0;
-    while (i < 3)
-    {
-        if (version[i] != static_num[i])
-            return (0);
-        i++;
-    }
-    return (1);
-
-}
+/**
+ * blockchain_deserialize - Deserialize a blockchain from a file
+ * @path: Path to the file containing serialized blockchain data
+ *
+ * Return: Pointer to the deserialized blockchain
+ */
 
 blockchain_t *blockchain_deserialize(char const *path)
 {
-    FILE *fp;
-    block_t *block;
-    block_info_t block_info;
-    block_data_t block_data;
-    blockchain_t *blockchain = calloc(1, sizeof(blockchain_t));
-    uint8_t num_blocks = 0, i;
-    unsigned char four_[4];
-    
-    blockchain->chain = llist_create(MT_SUPPORT_FALSE);
-    fp = fopen(path, "rb");
-    if (!fp)
-        return (NULL);
-    fread(four_, 1, 4, fp);
-    if (!check_num(four_))
-        return (NULL);
-    fread(four_, 1, 3, fp);
-    if (!check_version(four_))
-        return (NULL);
-    fread(four_, 1, 1, fp);
-    if (_get_endianness() != four_[0])
-        puts("endianness does not match\n");
-    fread(&num_blocks, 1, 4, fp);
-    for (i = 0; i < num_blocks; i++)
-    {
-        block = calloc(1 , sizeof(block_t));
-        fread(&block_info.index, 1, 4, fp);
-        fread(&block_info.difficulty, 1, 4, fp);
-        fread(&block_info.timestamp, 1, 8, fp);
-        fread(&block_info.nonce, 1, 8, fp);
-        fread(&block_info.prev_hash, 1 ,32, fp);
-        fread(&block_data.len, 1, 4, fp);
-        fread(block_data.buffer, 1, block_data.len, fp);
-        fread(block->hash, 1, 32, fp);
-        block->data = block_data;
-        block->info = block_info;
-        llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
-    }
+	FILE *fd = fopen(path, "r");
+	char buf[4] = {0};
+	uint8_t end;
+	uint32_t size;
+	blockchain_t *blockchain = calloc(1, sizeof(blockchain_t));
 
-    return (blockchain);
 
-    
+	if (!fd)
+		return (NULL);
+	fread(buf, sizeof(uint8_t), 4, fd);
+	fread(buf, sizeof(uint8_t), 3, fd);
+	fread(&end, sizeof(uint8_t), 1, fd);
+	fread(&size, sizeof(uint32_t), 1, fd);
+	blockchain->chain = llist_create(MT_SUPPORT_FALSE);
+	blockchain = write_blocks(size, fd, blockchain);
+	fclose(fd);
+
+	return (blockchain);
 }

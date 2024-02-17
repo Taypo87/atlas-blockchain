c64e85b4cbd43f2cc9e0ad95ff45ee2b968239cf
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-16 19:56:13 -0600

    task 2

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -7,7 +7,7 @@ void block_mine(block_t *block)
 {
 	uint32_t buff[SHA256_DIGEST_LENGTH];
 
-	block_hash(block, buff);
+	block_hash(block, &buff);
 
 	while (!hash_matches_difficulty(buff, block->info.difficulty))
 	{

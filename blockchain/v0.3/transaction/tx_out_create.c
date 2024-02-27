0f148e288fa623f6d67701a101646c1449f152b1
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-27 13:57:55 -0600

    changes sizeof for update calls

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -15,8 +15,8 @@ tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
     if (new == NULL)
         return (NULL);
     SHA256_Init(&ctx);
-    SHA256_Update(&ctx, &amount, EC_PUB_LEN);
-    SHA256_Update(&ctx, pub, EC_PUB_LEN);
+    SHA256_Update(&ctx, &amount, sizeof(amount));
+    SHA256_Update(&ctx, pub, sizeof(pub));
     SHA256_Final(new->hash, &ctx);
     new->amount = amount;
     memcpy(new->pub, pub, EC_PUB_LEN);

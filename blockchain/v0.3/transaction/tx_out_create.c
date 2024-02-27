9b4c237c843ed300f4b1c5ec209a9273caa548cf
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-27 15:50:12 -0600

    hashed the pub and amount seprately

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -10,18 +10,13 @@
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
     tx_out_t *new = calloc(1, sizeof(tx_out_t));
-    SHA256_CTX ctx;
 
     if (new == NULL)
         return (NULL);
     
     new->amount = amount;
     memcpy(new->pub, pub, EC_PUB_LEN);
-    SHA256_Init(&ctx);
-    SHA256_Update(&ctx, &amount, sizeof(amount));
-    SHA224_Update(&ctx, pub, sizeof(pub));
-    SHA256_Final(new->hash, &ctx);
-    
+    sha256((int8_t *)new, sizeof(new->amount) + sizeof(new->pub), new->hash);
     return (new);
 
 }

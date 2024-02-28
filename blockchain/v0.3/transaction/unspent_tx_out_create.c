1b1408ac6e7a4736ea1926d1adc76c4e4ef8c476
Taypo87 <taylor.poczynek87@gmail.com>
2024-02-28 02:14:56 -0600

    was trying to hash a hash, memcpy used  instead

--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -1,17 +1,18 @@
 #include "transaction.h"
 /**
- * unspent_tx_create - allocates and initializes a unspent_tx_out struct
- * @blockhash: the hash of the the newly created unspent_tx_out
+ * unspent_tx_out_create - allocates and initializes a unspent_tx_out struct
+ * @block_hash: the hash of the the newly created unspent_tx_out
  * @tx_id: the transaction id
  * @out: copy of the referenced transaction output
+ * Return: pointer to the newly createrd struct
 */
 unspent_tx_out_t *unspent_tx_out_create(uint8_t block_hash[SHA256_DIGEST_LENGTH],
-                        uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out)
+						uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out)
 {
-    unspent_tx_out_t *unspent = calloc(1, sizeof(unspent_tx_out_t));
+	unspent_tx_out_t *unspent = calloc(1, sizeof(unspent_tx_out_t));
 
-    unspent->out = *out;
-    memcpy(unspent->tx_id, tx_id, SHA256_DIGEST_LENGTH);
-    memcpy(unspent->block_hash, block_hash, SHA256_DIGEST_LENGTH);
+	unspent->out = *out;
+	memcpy(unspent->tx_id, tx_id, SHA256_DIGEST_LENGTH);
+	memcpy(unspent->block_hash, block_hash, SHA256_DIGEST_LENGTH);
 	return (unspent);
 }

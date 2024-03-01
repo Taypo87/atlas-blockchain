9ad6fc2a3e3ddb04695c1f4987a00eeb6a66fe19
Taypo87 <taylor.poczynek87@gmail.com>
2024-03-01 11:28:48 -0600

    was using incorrect sutruct for llistget

--- blockchain/v0.3/transaction/tx_in_sign.c
+++ blockchain/v0.3/transaction/tx_in_sign.c
@@ -1,6 +1,5 @@
 #include "transaction.h"
 
-
 /**
  * tx_in_sign - signs a transaction input
  * @in: transaction input structure
@@ -13,17 +12,19 @@ sig_t *tx_in_sign(tx_in_t *in,
 	uint8_t const tx_id[SHA256_DIGEST_LENGTH],
 		EC_KEY const *sender, llist_t *all_unspent)
 {
-	 uint8_t pub_buf[EC_PUB_LEN];
-	 tx_out_t *node;
-	 sig_t *sig;
-	 int i, list_size;
+	uint8_t pub_buf[EC_PUB_LEN];
+	unspent_tx_out_t *node;
+	sig_t *sig;
+	int i, list_size;
+
 
 	list_size = llist_size(all_unspent);
+	printf("list size is: %d\n", list_size);
 	ec_to_pub(sender, pub_buf);
 	for (i = 0; i < list_size; i++)
 	{
 		node = llist_get_node_at(all_unspent, i);
-		if (memcmp(node->pub, pub_buf, EC_PUB_LEN) == 0)
+		if (memcmp(node->out.pub, pub_buf, EC_PUB_LEN) == 0)
 			{
 				ec_sign(sender, tx_id, SHA256_DIGEST_LENGTH, &in->sig);
 				sig = &in->sig;

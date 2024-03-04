7f7b4cce48f2e6bfc2fa6721261a2833fb7571c1
Taypo87 <taylor.poczynek87@gmail.com>
2024-03-04 03:51:40 -0600

    removed size check

--- blockchain/v0.3/transaction/transaction_is_valid.c
+++ blockchain/v0.3/transaction/transaction_is_valid.c
@@ -9,14 +9,13 @@ int transaction_is_valid(transaction_t const *transaction,
 										llist_t *all_unspent)
 {
 	uint8_t buffer[SHA256_DIGEST_LENGTH], *flags;
-	int i, input_size, output_size, j, unspent_size;
+	int i, input_size, j, unspent_size;
 	tx_in_t *in_node;
 	unspent_tx_out_t *unspent_node;
 	EC_KEY *pub_key;
 	
 	unspent_size = llist_size(all_unspent);
 	input_size = llist_size(transaction->inputs);
-	output_size = llist_size(transaction->outputs);
 	flags = calloc(input_size, sizeof(uint8_t));
 	transaction_hash(transaction, buffer);
 	if (memcmp(&buffer, transaction->id, SHA256_DIGEST_LENGTH) != 0)

1050304f5442b0cc6990c2669a7e191a372cd3e4
Taypo87 <taylor.poczynek87@gmail.com>
2024-03-04 06:00:28 -0600

    debugging

--- blockchain/v0.3/transaction/transaction_is_valid.c
+++ blockchain/v0.3/transaction/transaction_is_valid.c
@@ -38,7 +38,7 @@ int transaction_is_valid(transaction_t const *transaction,
 			}
 		}
 		pub_key = ec_from_pub(unspent_node->out.pub);
-		if (!ec_verify(pub_key, transaction->id, SHA256_DIGEST_LENGTH, &in_node->sig))
+		if (ec_verify(pub_key, transaction->id, SHA256_DIGEST_LENGTH, &in_node->sig))
 			return (0);
 		EC_KEY_free(pub_key);
 		

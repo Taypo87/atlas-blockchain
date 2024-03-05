fbaf4960eb4df6fe811f2c2486a8e41c832bb784
Taypo87 <taylor.poczynek87@gmail.com>
2024-03-05 05:26:03 -0600

    betty

--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -6,7 +6,8 @@
  * @out: copy of the referenced transaction output
  * Return: pointer to the newly createrd struct
 */
-unspent_tx_out_t *unspent_tx_out_create(uint8_t block_hash[SHA256_DIGEST_LENGTH],
+unspent_tx_out_t *unspent_tx_out_create\
+(uint8_t block_hash[SHA256_DIGEST_LENGTH],
 		uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out)
 {
 	unspent_tx_out_t *unspent = calloc(1, sizeof(unspent_tx_out_t));

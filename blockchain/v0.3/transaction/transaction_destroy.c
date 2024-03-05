2c6998f2b34f2e803a7dcaee124c5429eeb50545
Taypo87 <taylor.poczynek87@gmail.com>
2024-03-05 04:07:45 -0600

    updated 2nd arg for llist destroy

--- blockchain/v0.3/transaction/transaction_destroy.c
+++ blockchain/v0.3/transaction/transaction_destroy.c
@@ -6,11 +6,6 @@
 */
 void transaction_destroy(transaction_t *transaction)
 {
-	int in_size, out_size;
-
-	in_size = llist_size(transaction->inputs);
-	out_size = llist_size(transaction->outputs);
-
 	llist_destroy(transaction->inputs, 1, free);
 	llist_destroy(transaction->outputs, 1, free);
 	free(transaction);

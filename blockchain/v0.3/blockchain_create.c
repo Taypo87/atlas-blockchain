75873989a00c244129574c0f322525bc49a18db4
Taypo87 <taylor.poczynek87@gmail.com>
2024-03-05 04:36:33 -0600

    betty

--- blockchain/v0.3/blockchain_create.c
+++ blockchain/v0.3/blockchain_create.c
@@ -9,11 +9,12 @@ blockchain_t *blockchain_create(void)
 {
 	blockchain_t *blockchain = calloc(1, sizeof(blockchain_t));
 	block_t *genesis_block = calloc(1, sizeof(block_t));
-	uint8_t data[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', ' ', 'S', 'c', 'h', 'o', 'o', 'l', '\0'};
+	uint8_t data[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', ' ',
+										 'S', 'c', 'h', 'o', 'o', 'l', '\0'};
 
 	genesis_block->info.timestamp = 1537578000;
 
-	memset(genesis_block->info.prev_hash, 0, BLOCKCHAIN_DATA_MAX); 
+	memset(genesis_block->info.prev_hash, 0, BLOCKCHAIN_DATA_MAX);
 
 	genesis_block->data.len = strlen((char *)(data));
 	memcpy(genesis_block->data.buffer, data, genesis_block->data.len);

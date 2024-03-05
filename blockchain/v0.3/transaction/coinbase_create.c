8e77fe46af43a49f7fbf1510506790d287d8a3a8
Taypo87 <taylor.poczynek87@gmail.com>
2024-03-05 02:36:18 -0600

    was passing the address of in and out, changed that to passing directly

--- blockchain/v0.3/transaction/coinbase_create.c
+++ blockchain/v0.3/transaction/coinbase_create.c
@@ -6,22 +6,18 @@ transaction_t *coinbase_create(EC_KEY const *receiver, uint32_t block_index)
     tx_out_t *out_P = calloc(1, sizeof(tx_out_t));
     tx_in_t *in_p = calloc(1, sizeof(tx_in_t));
     uint8_t pub_buf[EC_PUB_LEN];
-    llist_t *in_list, *out_list;
+
 
     if(!receiver)
         return (NULL);
-
-    memcpy(in_p->tx_out_hash, &block_index, 4);
     out_P->amount = COINBASE_AMOUNT;
     ec_to_pub(receiver, pub_buf);
     memcpy(out_P->pub, pub_buf, 32);
     sha256((int8_t *)out_P, sizeof(out_P->amount) + sizeof(out_P->pub), out_P->hash);
-    in_list = llist_create(MT_SUPPORT_FALSE);
-    out_list = llist_create(MT_SUPPORT_FALSE);
-    llist_add_node(in_list, &in_p, ADD_NODE_FRONT);
-    llist_add_node(out_list, &out_P, ADD_NODE_FRONT);
-    transaction->inputs = in_list;
-    transaction->outputs = out_list;
+    transaction->inputs = llist_create(MT_SUPPORT_FALSE);
+    transaction->outputs = llist_create(MT_SUPPORT_FALSE);
+    llist_add_node(transaction->inputs, in_p, ADD_NODE_FRONT);
+    llist_add_node(transaction->outputs, out_P, ADD_NODE_FRONT);
     transaction_hash(transaction, pub_buf);
     memcpy(transaction->id, pub_buf, 32);
     return(transaction);

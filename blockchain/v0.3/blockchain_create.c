4b031bfb135468feab5ac5d45b8af581caa64b10
Taypo87 <taylor.poczynek87@gmail.com>
2024-03-05 04:34:30 -0600

    betty

--- blockchain/v0.3/blockchain_create.c
+++ blockchain/v0.3/blockchain_create.c
@@ -14,11 +14,13 @@ blockchain_t *blockchain_create(void)
 	genesis_block->info.timestamp = 1537578000;
 
 	memset(genesis_block->info.prev_hash, 0, BLOCKCHAIN_DATA_MAX); 
-	
+
 	genesis_block->data.len = strlen((char *)(data));
 	memcpy(genesis_block->data.buffer, data, genesis_block->data.len);
 
-	memcpy(genesis_block->hash, "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03", SHA256_DIGEST_LENGTH);
+	memcpy(genesis_block->hash, "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d \
+	\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff \
+	\x2f\x04\x51\x58\x03", SHA256_DIGEST_LENGTH);
 
 	blockchain->chain = llist_create(MT_SUPPORT_TRUE);
 	llist_add_node(blockchain->chain, genesis_block, ADD_NODE_FRONT);

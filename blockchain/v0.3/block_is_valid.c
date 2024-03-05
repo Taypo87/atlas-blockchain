f16a1da85bbbacfe1dcf6686886c61b9acd559cb
Taypo87 <taylor.poczynek87@gmail.com>
2024-03-05 05:07:08 -0600

    task 13

--- blockchain/v0.3/block_is_valid.c
+++ blockchain/v0.3/block_is_valid.c
@@ -8,12 +8,13 @@
  * Return: 0 if the block is valid, 1 otherwise
  */
 
-int block_is_valid(block_t const *block, block_t const *prev_block)
+int block_is_valid(block_t const *block, block_t const *prev_block, llist_t *all_unspent)
 {
 	uint8_t hash_buf[SHA256_DIGEST_LENGTH] = {0};
 	blockchain_t *genesis;
 	block_t *first;
 	int check;
+	transaction_t *transaction;
 
 	if (!block || (!prev_block && block->info.index != 0))
 		return (1);
@@ -39,6 +40,10 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 		memcmp(hash_buf, block->hash, SHA256_DIGEST_LENGTH))
 		return (1);
 	if (block->data.len > BLOCKCHAIN_DATA_MAX)
+		return (1);
+	
+	transaction = llist_get_node_at(all_unspent, 0);
+	if (!transaction_is_valid(transaction, all_unspent))
 		return (1);
 
 	return (hash_matches_difficulty(block->hash, block->info.difficulty) ? 0 : 1);

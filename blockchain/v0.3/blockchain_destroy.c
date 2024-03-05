1d93fca533bd59c9552f23edcbb78742e7b314e2
Taypo87 <taylor.poczynek87@gmail.com>
2024-03-05 04:13:20 -0600

    trying something

--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -7,6 +7,6 @@
 void blockchain_destroy(blockchain_t *blockchain)
 {
 	llist_destroy((llist_t *)blockchain->chain,
-		llist_size((llist_t *)blockchain->chain), block_destroy);
+		1, (node_dtor_t)block_destroy);
 	free(blockchain);
 }
